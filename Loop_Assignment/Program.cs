d03e65cb5d0ddaac2d5ec2c1e34a4dbfbc5be903
Margarita <margaritakrasnauska@gmail.com>
2023-01-17 10:27:42 +0000

    Add project files.

---
+++ Loop_Assignment/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loop_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Type a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            while (number > 15)
+            {
+                Console.WriteLine("Please try again");
+                Console.WriteLine("Please Type a number");
+                number = Convert.ToInt32(Console.ReadLine());
+                Console.ReadLine();
+
+
+            }
+            Console.WriteLine("Select a number for countdown:");
+
+            int countNum = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                Console.WriteLine(countNum);
+                countNum--;
+            }
+            while (countNum > 0);
+            Console.ReadLine();
+        }
+    }
+}
